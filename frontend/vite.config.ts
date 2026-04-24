db92feb60cd8b82239efdcc47d74943f33cba982
Haider Bassem <haideralatbe@gmail.com>
2026-04-24 22:09:07 +0300

    fix: routrs

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,13 +3,24 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from "path"
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: '192.168.16.138',
+    port: 5173,
+  },
+  preview: {
+    host: '192.168.16.138',
+    port: 4173,
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    minify: 'oxc',
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
 })
-
